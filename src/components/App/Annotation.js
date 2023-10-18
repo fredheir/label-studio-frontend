5f2c113c30c5cd9eefde1fc57cd14b821d725db5
fredheir <fredheir@gmail.com>
2023-10-18 15:51:02 +0300

    add Twitter js

--- src/components/App/Annotation.js
+++ src/components/App/Annotation.js
@@ -4,10 +4,17 @@ import { useLayoutEffect } from 'react';
 
 export function Annotation({ annotation, root }) {
   useLayoutEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.charset = 'utf-8';
+    document.body.appendChild(script);
+
     return () => {
       if (annotation && isAlive(annotation)) {
         annotation.resetReady();
       }
+      document.body.removeChild(script);
     };
   }, [annotation.pk, annotation.id]);
   return root ? Tree.renderItem(root, annotation) : null;
